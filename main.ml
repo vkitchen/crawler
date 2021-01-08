8da96c82207a9554b87546164ce138f327fd35d9
Vaughan Kitchen <vkitchen@tophat.nz>
2021-01-08 17:30:52 +1300

    Proper paths

--- main.ml
+++ main.ml
@@ -18,6 +18,8 @@ let fetch (site : string) : unit t =
   Client.get (Uri.of_string site) >>= fun (_, body) ->
   body |> Cohttp_lwt.Body.to_string >>= fun b ->
     links b
+    |> List.filter (fun l -> String.index_opt l '#' <> Some 0)
+    |> List.map (fun l -> String.concat "/" (site :: l :: []) )
     |> List.iter print_endline;
     Lwt.return ()
 
