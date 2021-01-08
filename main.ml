8fa7d04be4e68d3450c77af0c39d607630bb5833
Vaughan Kitchen <vkitchen@tophat.nz>
2021-01-08 16:54:56 +1300

    Print anchor tags in fetched pages

--- main.ml
+++ main.ml
@@ -1,5 +1,6 @@
 open Lwt
 open Cohttp_lwt_unix
+open Soup
 
 let sites: string list =
   [ "http://vaughan.kitchen"
@@ -7,9 +8,14 @@ let sites: string list =
   ; "http://ambersong.me"
   ]
 
-let body (site : string) : unit t =
+let links (page : string) : unit =
+  let soup = parse page in
+  soup $$ "a[href]"
+  |> iter (fun a -> a |> R.attribute "href" |> print_endline)
+
+let fetch (site : string) : unit t =
   Client.get (Uri.of_string site) >>= fun (_, body) ->
-  body |> Cohttp_lwt.Body.to_string >|= print_endline
+  body |> Cohttp_lwt.Body.to_string >|= links
 
 let () =
-  Lwt_main.run (Lwt.join (List.map body sites))
+  Lwt_main.run (Lwt.join (List.map fetch sites))
