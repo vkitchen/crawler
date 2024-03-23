fdc174ce2e374b1e8fc7a3db7b1916c890103ee3
Vaughan Kitchen <contact@tophat.nz>
2024-03-23 13:59:22 +1300

    Fetch domains from db

--- src/crawler.zig
+++ src/crawler.zig
@@ -7,59 +7,63 @@ const std = @import("std");
 const zqlite = @import("zqlite");
 const curl = @cImport(@cInclude("curl/curl.h"));
 
-pub fn main() !void {
-    const flags = zqlite.OpenFlags.EXResCode;
-    var conn = try zqlite.open("crawler.db", flags);
-    defer conn.close();
+// 10MiB HTML response buffer
+var fixed_buffer: [10 * 1024 * 1024]u8 = undefined;
+var response_allocator = std.heap.FixedBufferAllocator.init(&fixed_buffer);
+var response_buffer = std.ArrayList(u8).init(response_allocator.allocator());
 
-    var rows = try conn.rows("select id, domain from sites", .{});
+fn curlCallback(data: *anyopaque, size: c_uint, nmemb: c_uint, user_data: *anyopaque) callconv(.C) c_uint {
+    const buffer: *std.ArrayList(u8) = @alignCast(@ptrCast(user_data));
+    const typed_data: [*]u8 = @ptrCast(data);
+    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
+    return nmemb * size;
+}
+
+fn fetch(curl_handle: *anyopaque, address: [*:0]const u8) !void {
+    try response_buffer.resize(0);
+
+    if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_URL, address) != curl.CURLE_OK)
+        return error.CURLSetURLFailed;
+
+    if (curl.curl_easy_perform(curl_handle) != curl.CURLE_OK)
+        return error.CURLPerformRequestFailed;
+}
+
+fn tick(db: zqlite.Conn, curl_handle: *anyopaque) !void {
+    var rows = try db.rows("select id, domain from sites", .{});
     defer rows.deinit();
     while (rows.next()) |row| {
-        std.debug.print("domain: {s}\n", .{row.text(1)});
+        const domain = row.textZ(1);
+
+        std.debug.print("domain: {s}\n", .{domain});
+        try fetch(curl_handle, domain);
+        std.log.info("Got response of {d} bytes", .{response_buffer.items.len});
+        std.debug.print("{s}\n", .{response_buffer.items});
     }
     if (rows.err) |err| return err;
+}
 
-    var arena_state = std.heap.ArenaAllocator.init(std.heap.c_allocator);
-    defer arena_state.deinit();
+pub fn main() !void {
+    // sqlite init
+    const flags = zqlite.OpenFlags.EXResCode;
+    var db = try zqlite.open("crawler.db", flags);
+    defer db.close();
 
-    const allocator = arena_state.allocator();
-
-    // global curl init, or fail
+    // curl init
     if (curl.curl_global_init(curl.CURL_GLOBAL_ALL) != curl.CURLE_OK)
         return error.CURLGlobalInitFailed;
     defer curl.curl_global_cleanup();
 
-    // curl easy handle init, or fail
-    const handle = curl.curl_easy_init() orelse return error.CURLHandleInitFailed;
-    defer curl.curl_easy_cleanup(handle);
+    const curl_handle = curl.curl_easy_init() orelse return error.CURLEasyInitFailed;
+    defer curl.curl_easy_cleanup(curl_handle);
 
-    var response_buffer = std.ArrayList(u8).init(allocator);
+    // set curl options
+    if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_WRITEFUNCTION, curlCallback) != curl.CURLE_OK)
+        return error.CURLSetOptFailed;
+    if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_WRITEDATA, &response_buffer) != curl.CURLE_OK)
+        return error.CURLSetOptFailed;
+    if (curl.curl_easy_setopt(curl_handle, curl.CURLOPT_USERAGENT, "Mozilla/5.0 (compatible; PotatoCastlesBot; +http://potatocastles.com)") != curl.CURLE_OK)
+        return error.CURLSetOptFailed;
 
-    // superfluous when using an arena allocator, but
-    // important if the allocator implementation changes
-    defer response_buffer.deinit();
-
-    // setup curl options
-    if (curl.curl_easy_setopt(handle, curl.CURLOPT_URL, "https://ziglang.org") != curl.CURLE_OK)
-        return error.CouldNotSetURL;
-
-    // set write function callbacks
-    if (curl.curl_easy_setopt(handle, curl.CURLOPT_WRITEFUNCTION, writeToArrayListCallback) != curl.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-    if (curl.curl_easy_setopt(handle, curl.CURLOPT_WRITEDATA, &response_buffer) != curl.CURLE_OK)
-        return error.CouldNotSetWriteCallback;
-
-    // perform
-    if (curl.curl_easy_perform(handle) != curl.CURLE_OK)
-        return error.FailedToPerformRequest;
-
-    std.log.info("Got response of {d} bytes", .{response_buffer.items.len});
-    std.debug.print("{s}\n", .{response_buffer.items});
-}
-
-fn writeToArrayListCallback(data: *anyopaque, size: c_uint, nmemb: c_uint, user_data: *anyopaque) callconv(.C) c_uint {
-    var buffer: *std.ArrayList(u8) = @alignCast(@ptrCast(user_data));
-    var typed_data: [*]u8 = @ptrCast(data);
-    buffer.appendSlice(typed_data[0 .. nmemb * size]) catch return 0;
-    return nmemb * size;
+    try tick(db, curl_handle);
 }
